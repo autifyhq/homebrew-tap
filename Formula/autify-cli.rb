3b8e564f0dd4d38272c9e09798f2bb6e5592a749
riywo <riywo@users.noreply.github.com>
2022-11-14 22:16:21 +0000

    [autify-cli] Release 0.19.0

--- Formula/autify-cli.rb
+++ Formula/autify-cli.rb
@@ -5,8 +5,8 @@
 class AutifyCli < Formula
   desc "Autify Command Line Interface (CLI)"
   homepage "https://github.com/autifyhq/autify-cli"
-  url "https://github.com/autifyhq/autify-cli", using: :git, revision: "4d677ee"
-  version "0.18.0"
+  url "https://github.com/autifyhq/autify-cli", using: :git, revision: "8a001f6"
+  version "0.19.0"
   # sha256 ""
   license "MIT"
 
