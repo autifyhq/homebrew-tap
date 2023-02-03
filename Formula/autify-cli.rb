a07acf16637161a40d5fc7bf7d47b7941a8d7fd0
hgsgtk <hgsgtk@users.noreply.github.com>
2023-02-03 01:14:23 +0000

    [autify-cli] Release 0.26.0

--- Formula/autify-cli.rb
+++ Formula/autify-cli.rb
@@ -5,8 +5,8 @@
 class AutifyCli < Formula
   desc "Autify Command Line Interface (CLI)"
   homepage "https://github.com/autifyhq/autify-cli"
-  url "https://github.com/autifyhq/autify-cli", using: :git, revision: "9b2b826"
-  version "0.24.0"
+  url "https://github.com/autifyhq/autify-cli", using: :git, revision: "1024b88"
+  version "0.26.0"
   # sha256 ""
   license "MIT"
 
