4bbb76857f52b8d271bdb3efd3e70359c19fc118
mtsmfm <mtsmfm@users.noreply.github.com>
2024-10-23 09:58:05 +0000

    [autify-cli] Release 0.49.0

--- Formula/autify-cli.rb
+++ Formula/autify-cli.rb
@@ -5,8 +5,8 @@
 class AutifyCli < Formula
   desc "Autify Command-Line Interface (CLI)"
   homepage "https://github.com/autifyhq/autify-cli"
-  url "https://github.com/autifyhq/autify-cli", using: :git, revision: "c7c6caf"
-  version "0.48.0"
+  url "https://github.com/autifyhq/autify-cli", using: :git, revision: "4981797"
+  version "0.49.0"
   # sha256 ""
   license "MIT"
 
